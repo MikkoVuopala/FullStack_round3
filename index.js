a03d410fb2104cc7892f42189f431648c8a81723
Mikko <mikko.vuopala98@gmail.com>
2020-10-21 18:13:10 +0300

    3.5 tehty

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
         id: 1,
@@ -23,6 +25,9 @@ let persons = [
         number: "39-23-6423122"
     }
 ]
+const generateId = () => {
+    return Math.floor(Math.random() * Math.floor(1000));
+}
 
 app.get('/api/persons', (req, res) => {
     res.json(persons)
@@ -48,6 +53,26 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id)
 
     response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
 })
 
 const PORT = 3001
